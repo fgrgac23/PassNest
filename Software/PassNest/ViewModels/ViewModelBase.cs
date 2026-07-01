a59fd3413a7d11930516f865e9def35036d11906
fgrgac23 <fgrgac23@foi.hr>
2026-07-02 01:40:48 +0200

    Kreiran Solution u folderu Software, kreiran Avalonia UI projekt za UI i dodani potrebni Nuggeti za rad s preview načinom.

---
+++ Software/PassNest/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PassNest.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ Software/PassNest/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PassNest.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
