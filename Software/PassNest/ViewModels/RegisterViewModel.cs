4545aec4c1b272b961206e058e76ec455e70cca3
fgrgac23 <fgrgac23@foi.hr>
2026-07-07 21:55:56 +0200

    Kreiran dizajn registracije.

---
+++ Software/PassNest/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,79 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PassNest.ViewModels;
+
+namespace PassNest.ViewModels
+{
+    public partial class RegisterViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private int currentStep = 1;
+
+        [ObservableProperty]
+        private string firstName = string.Empty;
+
+        [ObservableProperty]
+        private string lastName = string.Empty;
+
+        [ObservableProperty]
+        private string email = string.Empty;
+
+        [ObservableProperty]
+        private string masterPassword = string.Empty;
+
+        [ObservableProperty]
+        private string confirmMasterPassword = string.Empty;
+
+        [ObservableProperty]
+        private bool isPasswordRevealed;
+
+        [ObservableProperty]
+        private bool isConfirmPasswordRevealed;
+
+        public bool IsStep1Visible => CurrentStep == 1;
+        public bool IsStep2Visible => CurrentStep == 2;
+
+        public string RevealButtonText => IsPasswordRevealed ? "Hide" : "Reveal";
+
+        partial void OnCurrentStepChanged(int value)
+        {
+            OnPropertyChanged(nameof(IsStep1Visible));
+            OnPropertyChanged(nameof(IsStep2Visible));
+        }
+
+        partial void OnIsPasswordRevealedChanged(bool value)
+        {
+            OnPropertyChanged(nameof(RevealButtonText));
+        }
+
+        [RelayCommand]
+        private void NextStep()
+        {
+            CurrentStep = 2;
+        }
+
+        [RelayCommand]
+        private void PreviousStep()
+        {
+            CurrentStep = 1;
+        }
+
+        [RelayCommand]
+        private void TogglePasswordReveal()
+        {
+            IsPasswordRevealed = !IsPasswordRevealed;
+        }
+
+        [RelayCommand]
+        private void ToggleConfirmPasswordReveal()
+        {
+            IsConfirmPasswordRevealed = !IsConfirmPasswordRevealed;
+        }
+
+        [RelayCommand]
+        private void CreateValut()
+        {
+            // Implement registration logic here
+        }
+    }
+}
---
+++ Software/PassNest/Views/RegisterView.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls;
+
+namespace PassNest.Views
+{
+    public partial class RegisterView : UserControl
+    {
+        public RegisterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
