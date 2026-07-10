a6287244d9046977ec1edb14b21073c02284f901
fgrgac23 <fgrgac23@foi.hr>
2026-07-10 13:34:19 +0200

    Kreiran ViewModel koji će biti zajednički svim prozorima.

---
+++ Software/PassNest/ViewModels/ShellViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace PassNest.ViewModels
+{
+    public partial class ShellViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private string selectedNavItem = "Trezor";
+
+        [ObservableProperty]
+        private ViewModelBase currentPage;
+
+        public bool IsTrezorActive => SelectedNavItem == "Trezor";
+        public bool IsGeneratorActive => SelectedNavItem == "Generator";
+        public bool IsSafetyActive => SelectedNavItem == "Sigurnost";
+        public bool IsSettingsActive => SelectedNavItem == "Postavke";
+
+        public ShellViewModel()
+        {
+            currentPage = new VaultViewModel();
+        }
+
+        partial void OnSelectedNavItemChanged(string value)
+        {
+            OnPropertyChanged(nameof(IsTrezorActive));
+            OnPropertyChanged(nameof(IsGeneratorActive));
+            OnPropertyChanged(nameof(IsSafetyActive));
+            OnPropertyChanged(nameof(IsSettingsActive));
+
+            CurrentPage = value switch
+            {
+                "Trezor" => new VaultViewModel(),
+                _ => currentPage
+            }; 
+        }
+
+        [RelayCommand]
+        private void SelectNav(string item) => SelectedNavItem = item;
+
+        [RelayCommand]
+        private void LockVault()
+        {
+        }
+    }
+}
