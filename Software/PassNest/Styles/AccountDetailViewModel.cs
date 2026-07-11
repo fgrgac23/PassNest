0cb2abc367b81717459b21dc8fe791bf3c73c170
fgrgac23 <fgrgac23@foi.hr>
2026-07-11 15:50:17 +0200

    Kreirana klasa AccountDetailsViewModel.

---
+++ Software/PassNest/Styles/AccountDetailViewModel.cs
@@ -0,0 +1,89 @@
+﻿using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PassNest.ViewModels;
+
+namespace PassNest.Styles
+{
+    public partial class AccountDetailViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private string initial = "G";
+
+        [ObservableProperty]
+        private IBrush avatarColor = new SolidColorBrush(Color.Parse("#1B1F24"));
+
+        [ObservableProperty]
+        private string serviceName = "Github";
+
+        [ObservableProperty]
+        private string categoryName = "Posao";
+
+        [ObservableProperty]
+        private IBrush categoryColor = new SolidColorBrush(Color.Parse("#7C5CD6"));
+
+        [ObservableProperty]
+        private IBrush categoryTint = new SolidColorBrush(Color.Parse("#7C5CD6"), 0.15);
+
+        [ObservableProperty]
+        private string strengthLabel = "Jaka lozinka";
+
+        [ObservableProperty]
+        private IBrush strengthColor = new SolidColorBrush(Color.Parse("#2AA26A"));
+
+        [ObservableProperty]
+        private string username = "iivic";
+
+        [ObservableProperty]
+        private string password = "lozinka123";
+
+        [ObservableProperty]
+        private bool isPasswordRevealed;
+
+        [ObservableProperty]
+        private string url = "guthub.com/login";
+
+        [ObservableProperty]
+        private string lastModified = "12.06.2026.";
+
+        [ObservableProperty]
+        private string createdAt = "03.01.2025.";
+
+        public string MaskedPassword => new string('*', Password?.Length ?? 0);
+
+        partial void OnPasswordChanged(string value)
+        {
+            OnPropertyChanged(nameof(MaskedPassword));
+        }
+
+        [RelayCommand]
+        private void Edit()
+        {
+        }
+
+        [RelayCommand]
+        private void Delete()
+        {
+        }
+
+        [RelayCommand]
+        private void AutoFill()
+        {
+        }
+
+        [RelayCommand]
+        private void CopyUsername()
+        {
+        }
+
+        [RelayCommand]
+        private void CopyPassword()
+        {
+        }
+
+        [RelayCommand]
+        private void TogglePasswordReveal()
+        {
+        }
+    }
+}
