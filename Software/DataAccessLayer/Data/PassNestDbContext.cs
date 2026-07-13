44ffe3d7c8ef3fcaad6eda7a04c087aebcaff2e4
fgrgac23 <fgrgac23@foi.hr>
2026-07-13 20:05:18 +0200

    Kreirana klasa PassNestDbContext za kreiranje konteksta baze podataka.

---
+++ Software/DataAccessLayer/Data/PassNestDbContext.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Data
+{
+    public class PassNestDbContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Account> Accounts => Set<Account>();
+        public DbSet<Category> Categories => Set<Category>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PassNest");
+            Directory.CreateDirectory(folder);
+            var dbPath = Path.Combine(folder, "passnest.db");
+            options.UseSqlite($"Data Source={dbPath}");
+        }
+    }
+}
