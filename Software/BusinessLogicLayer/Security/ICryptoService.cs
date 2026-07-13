c7a62c15152b0368d6f3e9e23a890f3d001af5ea
fgrgac23 <fgrgac23@foi.hr>
2026-07-14 01:08:32 +0200

    Kreirano sučelje ICryptoService

---
+++ Software/BusinessLogicLayer/Security/ICryptoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Security
+{
+    public interface ICryptoService
+    {
+        string GenerateSalt();
+        string HashPassword(string password, string salt);
+        bool VerifyPassword(string password, string hash, string salt);
+        byte[] DeriveKey(string password, string salt);
+        string Encrypt(string plainText, byte[] key);
+        string Decrypt(string cipherText, byte[] key);
+    }
+}
