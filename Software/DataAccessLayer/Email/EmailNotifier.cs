38e69d1c0f0b9b7a1fc730f6eb64cee4f8864ce0
fgrgac23 <fgrgac23@foi.hr>
2026-07-14 01:05:31 +0200

    Kreirana komponenta za slanje koda za 2FA.

---
+++ Software/DataAccessLayer/Email/EmailNotifier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Email
+{
+    public class EmailNotifier : IEmailSender
+    {
+        private readonly string SmtpHost;
+        private readonly int SmtpPort;
+        private readonly string SenderEmail;
+        private readonly string SenderPassword;
+
+        public EmailNotifier(string smtpHost, int smtpPort, string senderEmail, string senderPassword)
+        {
+            SmtpHost = smtpHost;
+            SmtpPort = smtpPort;
+            SenderEmail = senderEmail;
+            SenderPassword = senderPassword;
+        }
+        public void SendEmail(string to, string subject, string body)
+        {
+            using var client = new SmtpClient(SmtpHost, SmtpPort)
+            {
+                Credentials = new NetworkCredential(SenderEmail, SenderPassword),
+                EnableSsl = true
+            };
+
+            using var message = new MailMessage(SenderEmail, to, subject, body);
+            client.Send(message);
+        }
+    }
+}
