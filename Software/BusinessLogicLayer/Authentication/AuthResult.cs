0d1ce551b9081b71d92eacaf2ae8496a9a6f5097
fgrgac23 <fgrgac23@foi.hr>
2026-07-14 00:44:11 +0200

    Kreirana klasa AuthResult i sučelje IAuthProvider.

---
+++ Software/BusinessLogicLayer/Authentication/AuthResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Authentication
+{
+    public class AuthResult
+    {
+        public bool Success { get; init; }
+        public bool RequiresTwoFactor { get; init; }
+        public string ErrorMessage { get; init; } = string.Empty;
+
+        public static AuthResult Ok(bool requiresTwoFactor = false) => new()
+        {
+            Success = true,
+            RequiresTwoFactor = requiresTwoFactor
+        };
+
+        public static AuthResult Fail(string errorMessage ) => new()
+        {
+            Success = false,
+            ErrorMessage = errorMessage
+        };
+    }
+}
---
+++ Software/BusinessLogicLayer/Authentication/IAtuhProvider.cs
@@ -0,0 +1,16 @@
+﻿using EntityLayer;
+
+namespace BusinessLogicLayer.Authentication
+{
+    public interface IAtuhProvider
+    {
+        bool HasRegisteredUser();
+        AuthResult RegisterUser(string name, string surname, string masterPassword);
+        AuthResult Login(string masterPassword);
+        void EnableTwoFactor(string email);
+        bool VerifyTwoFactor(string code);
+        User? GetCurrentUser();
+        byte[]? GetEncryptionKey();
+        void Logout();
+    }
+}
