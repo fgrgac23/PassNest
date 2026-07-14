3a304f23766296699b1a4c6390f9e9d632b7a5cc
fgrgac23 <fgrgac23@foi.hr>
2026-07-14 17:12:17 +0200

    Kreirana enumeracija za jačinu lozinke.

---
+++ Software/BusinessLogicLayer/PasswordGeneration/PasswordStrengthLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.PasswordGeneration
+{
+    public enum PasswordStrengthLevel
+    {
+        VrloSlaba,
+        Slaba,
+        Srednja,
+        Jaka
+    }
+}
