c81f5678aa706bc1421b85521df1300a2fea78f2
fgrgac23 <fgrgac23@foi.hr>
2026-07-14 15:26:16 +0200

    Dorađen MainWindowViewModel.

--- Software/PassNest/ViewModels/MainWindowViewModel.cs
+++ Software/PassNest/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,50 @@
-﻿namespace PassNest.ViewModels
+﻿using BusinessLogicLayer.Authentication;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+
+namespace PassNest.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        private readonly IAuthProvider authProvider;
+
+        [ObservableProperty]
+        private ViewModelBase currentPage;
+
+        public MainWindowViewModel(IAuthProvider authProvider)
+        {
+            this.authProvider = authProvider;
+            CurrentPage = CreateInitialPage();
+        }
+
+        private ViewModelBase CreateInitialPage() => authProvider.HasRegisteredUser() ? CreateLoginPage() : (ViewModelBase)CreateRegisterPage();
+
+        private LoginViewModel CreateLoginPage()
+        {
+            var vm = new LoginViewModel(authProvider);
+            vm.LoginSucceded += OnAuthenticated;
+            vm.TwoFacotrRequired += OnTwoFactorRequired;
+            return vm;
+        }
+
+        private void OnTwoFactorRequired()
+        {
+            var vm = new TwoFactorViewModel(authProvider);
+            vm.VerificationSucceeded += OnAuthenticated;
+            vm.BackRequested += () => CurrentPage = CreateLoginPage();
+            CurrentPage = vm;
+        }
+
+        private RegisterViewModel CreateRegisterPage()
+        {
+            var vm = new RegisterViewModel(authProvider);
+            vm.RegistrationSucceeded += OnAuthenticated;
+            return vm;
+        }
+
+        private void OnAuthenticated()
+        {
+            CurrentPage = new ShellViewModel();
+        }
     }
 }
