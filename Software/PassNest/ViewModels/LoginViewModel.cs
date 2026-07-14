47be80603d6d42311331def775e939d1b3273672
fgrgac23 <fgrgac23@foi.hr>
2026-07-14 14:58:28 +0200

    Dorađen LoginViewModel tako da koristi metode iz BLL-a.

--- Software/PassNest/ViewModels/LoginViewModel.cs
+++ Software/PassNest/ViewModels/LoginViewModel.cs
@@ -1,15 +1,30 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using BusinessLogicLayer.Authentication;
+using System;
 
 namespace PassNest.ViewModels
 {
     public partial class LoginViewModel : ViewModelBase
     {
+        private readonly IAtuhProvider authProvider;
+
         [ObservableProperty]
         private string masterPassword = string.Empty;
 
         [ObservableProperty]
         private bool isPasswordRevealed;
+
+        [ObservableProperty]
+        private string? errorMessage;
+
+        public event Action? LoginSucceded;
+        public event Action? TwoFacotrRequired;
+
+        public LoginViewModel(IAtuhProvider authProvider)
+        {
+            this.authProvider = authProvider;
+        }
 
         [RelayCommand]
         private void TogglePasswordReveal()
@@ -20,6 +35,22 @@ namespace PassNest.ViewModels
         [RelayCommand]
         private void Unlock()
         {
+            ErrorMessage = null;
+            var resutl = authProvider.Login(MasterPassword);
+
+            if (!resutl.Success)
+            {
+                ErrorMessage = resutl.ErrorMessage;
+                return;
+            }
+
+            if (resutl.RequiresTwoFactor)
+            {
+                TwoFacotrRequired?.Invoke();
+                return;
+            }
+
+            LoginSucceded?.Invoke();
         }
     }
 }
