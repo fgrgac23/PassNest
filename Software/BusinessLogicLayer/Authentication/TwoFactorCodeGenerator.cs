edd836b66500df18539264a4d6cde490985a06d4
fgrgac23 <fgrgac23@foi.hr>
2026-07-14 14:28:50 +0200

    Kreirana klasa za generiranje koda za 2FA.

---
+++ Software/BusinessLogicLayer/Authentication/TwoFactorCodeGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Authentication
+{
+    public class TwoFactorCodeGenerator
+    {
+        private readonly int CodeLength;
+
+        public TwoFactorCodeGenerator(int codeLength = 6)
+        {
+            CodeLength = codeLength;
+        }
+
+        public string GenerateCode()
+        {
+            var max = (int)Math.Pow(10, CodeLength);
+            
+            return RandomNumberGenerator.GetInt32(0, max).ToString().PadLeft(CodeLength, '0');
+        }
+
+        public bool ValidateCode(string input, string expected) => input == expected;
+    }
+}
