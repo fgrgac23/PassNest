e4eaae68b7dfbfa1fb00335f3bb95c408bdf0b8e
fgrgac23 <fgrgac23@foi.hr>
2026-07-15 11:22:49 +0200

    Kreirano sučelje IAccountStore.

---
+++ Software/BusinessLogicLayer/AccountManagement/IAccountStore.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.AccountManagement
+{
+    public interface IAccountStore
+    {
+        void AddAccount(string serviceName, string userName, string password, IEnumerable<int> categoryIds);
+        void UpdateAccount(string serviceName, string userName, string password, IEnumerable<int> categoryIds);
+        void DeleteAccount(int accountId);
+        IEnumerable<Account> GetAllAccounts();
+        IEnumerable<Account> SearchAccounts(string query);
+        IEnumerable<Account> FilterByCategory(int categoryId);
+        IEnumerable<Account> GetCategories();
+        Category AddCategory(string name, string color);
+        AccountCredentials? GetCredentials(int accountId);
+        IEnumerable<AccountCredentials> GetAllCredentials();
+    }
+}
