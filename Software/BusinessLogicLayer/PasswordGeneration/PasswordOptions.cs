6431e4474da4fee1366b3ba677429a3dde0cf91f
fgrgac23 <fgrgac23@foi.hr>
2026-07-20 18:25:41 +0200

    Kreirana klasa PasswordOptions za mogućnosti koje nudi generator lozinki.

---
+++ Software/BusinessLogicLayer/PasswordGeneration/PasswordOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.PasswordGeneration
+{
+    public class PasswordOptions
+    {
+        public int Length { get; set; }
+        public bool UseUppercase { get; set; }
+        public bool UseLowercase { get; set; }
+        public bool UseDigits { get; set; }
+        public bool UseSpecialChars { get; set; }
+    }
+}
