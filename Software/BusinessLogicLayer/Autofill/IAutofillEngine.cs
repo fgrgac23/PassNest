52a70f63dfcbd7d9d6802547fce6dd8b4f92178c
fgrgac23 <fgrgac23@foi.hr>
2026-07-21 15:22:04 +0200

    Kreirano sučelje za komponentu AutofillEngine.

---
+++ Software/BusinessLogicLayer/Autofill/IAutofillEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Autofill
+{
+    public interface IAutofillEngine
+    {
+        event Action? HotkeyPressed;
+
+        void RegisterHotkeys();
+        void UnregisterHotkeys();
+        bool TriggerAutofill(int accountId);
+    }
+}
