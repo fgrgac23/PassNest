f6b734acab76edfef303ab522b360f7655a59351
fgrgac23 <fgrgac23@foi.hr>
2026-07-22 23:36:58 +0200

    Kreirano sučelje IPasswordAuditor sa WeakPasswordEntry klasom koji će "pamtiti" loše lozinke.

---
+++ Software/BusinessLogicLayer/PasswordAudit/WeakPasswordEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.PasswordAudit
+{
+    public class WeakPasswordEntry
+    {
+        public int AccountId { get; init; }
+        public string ServiceName { get; init; } = string.Empty;
+        public string Reason { get; init; } = string.Empty;
+    }
+}
