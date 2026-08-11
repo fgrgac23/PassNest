eb34a2119a3e43a10c330e9103000e914ec6fa7a
fgrgac23 <fgrgac23@foi.hr>
2026-08-11 12:35:19 +0200

    Kreirana pomoćna klasa za mock podataka i EncryptionEngineTests klasa za testiranje komponente EncryptionEngine.

---
+++ Software/BusinessLogicLayer.Tests/EncryptionEngineTests.cs
@@ -0,0 +1,119 @@
+﻿using BusinessLogicLayer.Security;
+using System.Security.Cryptography;
+using Xunit;
+
+namespace BusinessLogicLayer.Tests
+{
+    public class EncryptionEngineTests
+    {
+        private readonly EncryptionEngine crypto = new();
+
+        [Fact]
+        public void GenerateSalt_ReturnsDifferentValueEachTime()
+        {
+            var salt1 = crypto.GenerateSalt();
+            var salt2 = crypto.GenerateSalt();
+
+            Assert.NotEqual(salt1, salt2);
+        }
+
+        [Fact]
+        public void HashPassword_SameInput_ProducesSameHash()
+        {
+            var salt = crypto.GenerateSalt();
+
+            var hash1 = crypto.HashPassword("MojaLozinka123!", salt);
+            var hash2 = crypto.HashPassword("MojaLozinka123!", salt);
+
+            Assert.Equal(hash1, hash2);
+        }
+
+        [Fact]
+        public void VerifyPassword_CorrectPassword_ReturnsTrue()
+        {
+            var salt = crypto.GenerateSalt();
+            var hash = crypto.HashPassword("MojaLozinka123!", salt);
+
+            Assert.True(crypto.VerifyPassword("MojaLozinka123!", hash, salt));
+        }
+
+        [Fact]
+        public void VerifyPassword_WrongPassword_ReturnsFalse()
+        {
+            var salt = crypto.GenerateSalt();
+            var hash = crypto.HashPassword("MojaLozinka123!", salt);
+
+            Assert.False(crypto.VerifyPassword("KrivaLozinka", hash, salt));
+        }
+
+        [Fact]
+        public void DeriveKey_SamePasswordAndSalt_ProducesSameKey()
+        {
+            var salt = crypto.GenerateSalt();
+
+            var key1 = crypto.DeriveKey("GlavnaLozinka1!", salt);
+            var key2 = crypto.DeriveKey("GlavnaLozinka1!", salt);
+
+            Assert.Equal(key1, key2);
+        }
+
+        [Fact]
+        public void DeriveKey_DifferentSalt_ProducesDifferentKey()
+        {
+            var key1 = crypto.DeriveKey("GlavnaLozinka1!", crypto.GenerateSalt());
+            var key2 = crypto.DeriveKey("GlavnaLozinka1!", crypto.GenerateSalt());
+
+            Assert.NotEqual(key1, key2);
+        }
+
+        [Fact]
+        public void DeriveKey_ReturnsKeyOfExpected256BitLength()
+        {
+            var key = crypto.DeriveKey("GlavnaLozinka1!", crypto.GenerateSalt());
+
+            Assert.Equal(32, key.Length);
+        }
+
+        [Fact]
+        public void EncryptDecrypt_RoundTrip_ReturnsOriginalPlainText()
+        {
+            var key = crypto.DeriveKey("GlavnaLozinka1!", crypto.GenerateSalt());
+            const string original = "korisnicka-lozinka-za-servis";
+
+            var encrypted = crypto.Encrypt(original, key);
+            var decrypted = crypto.Decrypt(encrypted, key);
+
+            Assert.Equal(original, decrypted);
+        }
+
+        [Fact]
+        public void Encrypt_SamePlainText_ProducesDifferentCipherTextEachTime()
+        {
+            var key = crypto.DeriveKey("GlavnaLozinka1!", crypto.GenerateSalt());
+
+            var cipher1 = crypto.Encrypt("ista-lozinka", key);
+            var cipher2 = crypto.Encrypt("ista-lozinka", key);
+
+            Assert.NotEqual(cipher1, cipher2);
+        }
+
+        [Fact]
+        public void Decrypt_WithWrongKey_ThrowsOrProducesDifferentText()
+        {
+            var key = crypto.DeriveKey("GlavnaLozinka1!", crypto.GenerateSalt());
+            var wrongKey = crypto.DeriveKey("DrugaLozinka2!", crypto.GenerateSalt());
+            const string original = "tajna-lozinka";
+
+            var encrypted = crypto.Encrypt(original, key);
+
+            try
+            {
+                var decrypted = crypto.Decrypt(encrypted, wrongKey);
+                Assert.NotEqual(original, decrypted);
+            }
+            catch (CryptographicException)
+            {
+            }
+        }
+    }
+}
---
+++ Software/BusinessLogicLayer.Tests/InMemoryRepository.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLayer.Repository;
+using System.Linq.Expressions;
+
+namespace BusinessLogicLayer.Tests
+{
+    public class InMemoryRepository<T> : IRepository<T> where T : class
+    {
+        private readonly List<T> items = new();
+        private readonly Func<T, int> getId;
+        private readonly Action<T, int> setId;
+        private int nextId = 1;
+
+        public InMemoryRepository(Func<T, int> getId, Action<T, int> setId)
+        {
+            this.getId = getId;
+            this.setId = setId;
+        }
+
+        public IReadOnlyList<T> Items => items;
+
+        public void Add(T entity)
+        {
+            setId(entity, nextId++);
+            items.Add(entity);
+        }
+
+        public void Delete(T entity) => items.Remove(entity);
+
+        public IEnumerable<T> GetAll(params Expression<Func<T, object>>[] includeProperties) => items;
+
+        public T? GetById(int id) => items.FirstOrDefault(i => getId(i) == id);
+
+        public void SaveChanges() { }
+
+        public void Update(T entity) { }
+
+        public void Clear() => items.Clear();
+    }
+}
