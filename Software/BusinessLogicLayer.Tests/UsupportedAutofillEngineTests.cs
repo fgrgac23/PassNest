370c4905b214d4ffdef25329d542c92440f73f7e
fgrgac23 <fgrgac23@foi.hr>
2026-08-11 13:39:56 +0200

    Kreirana klasa UnsupportedAutofillEngineTests za testiranje klase UnsupportedAutofillEngine.

---
+++ Software/BusinessLogicLayer.Tests/UsupportedAutofillEngineTests.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLogicLayer.Autofill;
+
+namespace BusinessLogicLayer.Tests
+{
+    public class UnsupportedAutofillEngineTests
+    {
+        private readonly UnsupportedAutofillEngine sut = new();
+
+        [Fact]
+        public void IsSupported_ReturnsFalse()
+        {
+            Assert.False(sut.IsSupported);
+        }
+
+        [Fact]
+        public void TriggerAutofill_AlwaysReturnsFalse()
+        {
+            Assert.False(sut.TriggerAutofill(1));
+        }
+
+        [Fact]
+        public void RegisterHotkeys_DoesNotThrow()
+        {
+            var exception = Record.Exception(() => sut.RegisterHotkeys());
+
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void UnregisterHotkeys_DoesNotThrow()
+        {
+            var exception = Record.Exception(() => sut.UnregisterHotkeys());
+
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void HotkeyPressed_NeverInvoked()
+        {
+            var wasInvoked = false;
+            sut.HotkeyPressed += () => wasInvoked = true;
+
+            sut.RegisterHotkeys();
+            sut.TriggerAutofill(1);
+
+            Assert.False(wasInvoked);
+        }
+    }
+}
