51d0c25194759e6ceeb88ffd6e22fc3b65b31d4d
fgrgac23 <fgrgac23@foi.hr>
2026-08-14 13:09:53 +0200

    Kreirana klasa UpdateCheckResult.

---
+++ Software/BusinessLogicLayer/UpdateCheck/UpdateCheckResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.UpdateCheck
+{
+    public class UpdateCheckResult
+    {
+        public bool IsUpdateAvailable { get; }
+        public string LatestVersion { get; }
+        public string ReleaseNotes { get; }
+        public string ReleaseUrl { get; }
+
+        public UpdateCheckResult(bool isUpdateAvailable, string latestVersion, string releaseNotes, string releaseUrl)
+        {
+            IsUpdateAvailable = isUpdateAvailable;
+            LatestVersion = latestVersion;
+            ReleaseNotes = releaseNotes;
+            ReleaseUrl = releaseUrl;
+        }
+
+        public static UpdateCheckResult NoUpdate() => new(false, string.Empty, string.Empty, string.Empty);
+
+        public static UpdateCheckResult Available(string latestVersion, string releaseNotes, string releaseUrl) => new(true, latestVersion, releaseNotes, releaseUrl);
+    }
+}
