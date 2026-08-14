bc039bdf0beb77a406f2b501c6730ca7704001ee
fgrgac23 <fgrgac23@foi.hr>
2026-08-14 13:13:48 +0200

    Kreirana klasa UpdateChecker s sučeljem IUpdateChecker.

---
+++ Software/BusinessLogicLayer/UpdateCheck/UpdateChecker.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLayer.UpdateCheck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.UpdateCheck
+{
+    public class UpdateChecker : IUpdateChecker
+    {
+        private readonly IUpdateSource updateSource;
+
+        public UpdateChecker(IUpdateSource updateSource)
+        {
+            this.updateSource = updateSource;
+        }
+
+        public async Task<UpdateCheckResult> CheckForUpdatesAsync(Version currentVersion)
+        {
+            var latestRelease = await updateSource.GetLatestReleaseAsync();
+            if (latestRelease == null) return UpdateCheckResult.NoUpdate();
+
+            var tag = latestRelease.TagName.TrimStart('v', 'V');
+            if(!Version.TryParse(tag, out var latestVersion))
+            {
+                return UpdateCheckResult.NoUpdate();
+            }
+
+            return latestVersion > currentVersion
+                ? UpdateCheckResult.Available(latestVersion.ToString(), latestRelease.Body, latestRelease.HtmlUrl)
+                : UpdateCheckResult.NoUpdate();
+        }
+    }
+}
