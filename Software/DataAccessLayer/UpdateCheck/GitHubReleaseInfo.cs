ee184d461318642624e8ebde37165b2b8b01f0e1
fgrgac23 <fgrgac23@foi.hr>
2026-08-14 12:57:11 +0200

    Kreirana klasa za prikupljanje informacija o verziji releasa.

---
+++ Software/DataAccessLayer/UpdateCheck/GitHubReleaseInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DataAccessLayer.UpdateCheck
+{
+    public class GitHubReleaseInfo
+    {
+        [JsonPropertyName("tag_name")]
+        public string TagName { get; set; } = string.Empty;
+
+        [JsonPropertyName("html_url")]
+        public string HtmlUrl { get; set; } = string.Empty;
+
+        [JsonPropertyName("body")]
+        public string Body { get; set; } = string.Empty;
+    }
+}
